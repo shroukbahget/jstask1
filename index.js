5d77db80583164545bbf2d87d67e7a546c31e72a
shroukbahget <Shroukabdallah922@gmail.com>
2024-08-11 23:15:18 -0700

    js-problems

---
+++ index.js
@@ -0,0 +1,112 @@
+// function checkCharacterCount() {
+//     var text = ("input");
+//     var numOfChars = text.length;
+
+//     if (numOfChars > 100) {
+//         console.log("String is long");
+//     } else {
+//         console.log("String is short");
+//     }
+// }
+// Input text
+// const text = "This is a sample text with more than 100 characters";
+
+// // Check the number of characters
+// if (text.length > 100) {
+//   console.log("The string is long");
+// } else {
+//   console.log("The string is short");
+// }
+
+// let number = 75;
+// if (number <= 100) {
+//   console.log("Number is less than or equal to 100");
+// } else {
+//   console.log("Number is greater than 100");
+// }
+
+// const text = "Enter your text here"; // Replace this with your text
+// const numCharacters = text.length;
+
+// if (numCharacters > 100) {
+//     console.log("The string is long");
+// } else {
+//     console.log("The string is short");
+// }
+
+
+// const today = new Date();
+// if (today.getDay() === 6) {
+//   console.log("It's the weekend!");
+// } else {
+//   console.log("It's a work day.");
+
+
+// function checkNumber(number) {
+//     if (number > 0) {
+//         console.log("The number is positive.");
+//     } else if (number < 0) {
+//         console.log("The number is negative.");
+//     } else {
+//         console.log("The number is zero.");
+//     }
+// }
+
+// function checkNumberSign(num) {
+//     if (num > 0) {
+//         return "Positive";
+//     } else if (num < 0) {
+//         return "Negative";
+//     } else {
+//         return "Zero";
+//     }
+// }
+
+
+// function getGrade(score) {
+//     if (score < 50) {
+//         return 'F';
+//     } else if (score < 60) {
+//         return 'E';
+//     } else if (score < 70) {
+//         return 'D';
+//     } else if (score < 80) {
+//         return 'C';
+//     } else if (score < 90) {
+//         return 'B';
+//     } else {
+//         return 'A';
+//     }
+// }
+
+
+// const temp = 25;
+
+// if (temp > 30) {
+//   console.log("It's hot");
+// } else {
+//   console.log("It's cold");
+// }
+
+
+// function checkOddEven(number) {
+//     if (number % 2 === 0) {
+//         console.log(`${number} is an even number.`);
+//     } else {
+//         console.log(`${number} is an odd number.`);
+//     }
+// }
+// checkOddEven();
+// checkOddEven();
+
+
+// const number = 25;
+
+// if (number % 2 === 0 && number > 20) {
+//     console.log("You can enter the university");
+// } else {
+//     console.log("You can't enter");
+// }
+
+
+
